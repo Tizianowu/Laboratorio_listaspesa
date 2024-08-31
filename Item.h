0bf3262f2fece22f17970fea9b64f29b96a12db2
Tizianowu <tiziano.wu@edu.unifi.it>
2024-08-31 16:59:30 +0200

    things forgot to commit

---
+++ Item.h
@@ -0,0 +1,15 @@
+//
+// Created by Tiziano Wu on 31/08/24.
+//
+
+#ifndef LABORATORIO_LISTASPESA_ITEM_H
+#define LABORATORIO_LISTASPESA_ITEM_H
+
+#endif //LABORATORIO_LISTASPESA_ITEM_H
+#include <wx/wx.h>
+struct Item{
+    wxString name;
+    int quantity;
+    Item(const wxString& itemName, int itemQuantity)
+            : name(itemName), quantity(itemQuantity) {}
+};
