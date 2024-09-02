1b16199518793eba42072b22daf1f36fa07c7bd6
Tizianowu <tiziano.wu@edu.unifi.it>
2024-09-03 00:11:46 +0200

    setting up Google Test

---
+++ cmake-build-debug/Testing/lib/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/tizianowu/Laboratorio_listaspesa/testing/lib/googletest/googletest
+# Build directory: /Users/tizianowu/Laboratorio_listaspesa/cmake-build-debug/testing/lib/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ testing/RunAllTest.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Tiziano Wu on 03/09/24.
+//
+
+#include "gtest/gtest.h"
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
